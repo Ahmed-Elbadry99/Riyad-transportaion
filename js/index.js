9fa640a14ed46cf5cf82750ee94d8e44b03fda2d
Mona Meshref <monameshref@gmail.com>
2025-11-04 16:05:00 -0800

    update-2

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 //! AOS library
 AOS.init();
+
+
+const sidebar = document.querySelector('.lisks-side-bar');
+let scrollTimer;
+
+sidebar.addEventListener('scroll', () => {
+    sidebar.classList.add('show-scroll');
+
+    clearTimeout(scrollTimer);
+    scrollTimer = setTimeout(() => {
+        sidebar.classList.remove('show-scroll');
+    }, 1000);
+});
