cedf7b19e4396ec26da9f7e0618974ece58ce4c2
Ahmed Elbadry <elbadry.egypt010@gmail.com>
2025-11-20 09:40:03 +0200

    lost page and smart matching

--- js/index.js
+++ js/index.js
@@ -28,25 +28,96 @@ closeIcon.addEventListener("click",function(){
     leftPart.classList.remove("responsive");
 });
 
+
+
 //! Dropdown Btn
 const dropdown = document.querySelector(".dropdown");
 const btn = document.getElementById("dropdownBtn");
 
-btn.addEventListener("click", () => {
-    dropdown.classList.toggle("active");
-});
+if(dropdown){
 
-// يقفل القايمة لما تدوسي براها 👇
+    
+    btn.addEventListener("click", () => {
+        dropdown.classList.toggle("active");
+    });
+    
+    // يقفل القايمة لما تدوسي براها 👇
+    window.addEventListener("click", (e) => {
+        if (!dropdown.contains(e.target)) {
+            dropdown.classList.remove("active");
+        }
+    });
+
+}    
+
+
+
+
+
+
+
+// losts table filter
+const filterBtn = document.getElementById("filter-btn");
+const filterContainer = document.querySelector(".filter-container");
+
+if(filterBtn){
+
+
+    
+filterBtn.addEventListener("click", function(){
+    filterContainer.classList.toggle("open")
+})
+
 window.addEventListener("click", (e) => {
-    if (!dropdown.contains(e.target)) {
-        dropdown.classList.remove("active");
+    if (!filterContainer.contains(e.target) && !filterBtn.contains(e.target)) {
+        filterContainer.classList.remove("open");
     }
 });
+}
 
 
 
 
+// progress 
+const progressBar = document.querySelector(".lost-progress .progress-bar");
+const steps = document.querySelectorAll(".lost-progress .step");
 
+function updateProgress() {
+    const doneCount = document.querySelectorAll(".lost-progress .step.done").length;
+    
+    progressBar.classList.remove("progress-50", "progress-75", "progress-100");
+    
+    if (doneCount === 1) {
+        progressBar.classList.add("progress-50");
+    } else if (doneCount === 2) {
+        progressBar.classList.add("progress-75");
+    } else if (doneCount === 3) {
+        progressBar.classList.add("progress-100");
+    }
+}
+
+updateProgress();
+
+
+function updateProgress1() {
+    // امسح أي next موجود قبل التحديث
+    steps.forEach(s => s.classList.remove("next"));
+
+    // هات آخر step واخدة done
+    const doneSteps = document.querySelectorAll(".lost-progress .step.done");
+    if (doneSteps.length === 0) return;
+
+    const lastDone = doneSteps[doneSteps.length - 1]; // آخر واحدة done
+    const lastDoneIndex = Array.from(steps).indexOf(lastDone);
+
+    // أضف next للخطوة اللي بعدها بس
+    const nextStep = steps[lastDoneIndex + 1];
+    if (nextStep) {
+        nextStep.classList.add("next");
+    }
+}   
+
+updateProgress1();
 
 
 
